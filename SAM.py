e4286c5ae4923c1922e6edc6f8de878c3c71dad4
PenagozDev <cristianpenagoso01@gmail.com>
2025-04-11 23:51:53 -0500

    Velocity calculation function

--- SAM.py
+++ SAM.py
@@ -59,7 +59,8 @@ cv2.createTrackbar("V Max", "Trackbars", 255, 255, nothing)
 def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1)** + (y2 - y1)**2)
 
-    
+
+#velocity calculation function
 
 
 
