b679dc2de4d0ae8285e23a7aa028915e2a32fd90
PenagozDev <cristianpenagoso01@gmail.com>
2025-04-11 23:48:52 -0500

    Euclidian distance formule

--- SAM.py
+++ SAM.py
@@ -56,6 +56,10 @@ cv2.createTrackbar("V Max", "Trackbars", 255, 255, nothing)
 
 #Centroid distannce calculation function
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)** + (y2 - y1)**2)
+
+    
 
 
 
